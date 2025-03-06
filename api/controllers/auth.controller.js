5e59b438f534159caf9875b6abc2514633fe3622
chrisnnaji001 <nnaji8546@gmail.com>
2025-03-06 13:19:13 +0100

    add middleware and function to handle errors

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,15 +1,33 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
 
-export const signup = async (req, res) =>{
+export const signup = async (req, res, next) => {
     const { username, email, password } =req.body;
-    const hashedPassword = bcryptjs.hashSync(password, 10);
-    const newUser = User({ username, email, password: hashedPassword });
-    try {
-        await newUser.save();
-        res.status(201).json('User created successfully!');
+  
+    if (
+      !username ||
+      !email ||
+      username === '' ||
+      email === '' ||
+      password === ''
+    ) {
 
-    } catch (error) {
-      next(error);  
-    }    
+      return res.status(400).json({ message: 'All fields are required'});
+    }
+
+    const hashedPassword = bcryptjs.hashSync(password, 10);
+
+const newUser = new User({
+  username,
+  email,
+  password: hashedPassword,
+}),
+
+try {
+  await newUser.save();
+  res.json('Signup successful');
+} catch (error) {
+  next(error);
+}
+
 };
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swe'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost;3000',
+        secure: false,
+      },
+    },
+  },
   plugins: [react()],
-})
+});
